416a1db4c311c182f8d341b7cd0c4b67f26b5d1c
Jeroen Berkvens <J.Berkvens@Moridrin.com>
2016-03-04 14:07:27 +0100

    Changed to Terminal project
    I started with a normal iOS application but we should have started with
    an OSX terminal application.

---
+++ Practica/HellGlow World/HellGlow World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HellGlow World
+//
+//  Created by Jeroen Berkvens on 04/03/16.
+//  Copyright © 2016 Fontys Universities. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
