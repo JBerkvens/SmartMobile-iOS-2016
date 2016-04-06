25755bcbae627699c2bda9c1b5b16c97d4b1a88a
Jeroen Berkvens <J.Berkvens@Moridrin.com>
2016-04-06 10:13:24 +0200

    Step 2

---
+++ Practica/json_test/json_test/Pirate.swift
@@ -0,0 +1,12 @@
+//
+//  Pirate.swift
+//  json_test
+//
+//  Created by Jeroen Berkvens on 06/04/16.
+//  Copyright © 2016 Fontys Universities. All rights reserved.
+//
+
+import Foundation
+class Pirate {
+    
+}
